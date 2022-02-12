b67869786a5754c6f075fc13a88c99c17b0581e4
rushi mungse <mungse.rushi@gmail.com>
2022-02-12 22:31:05 +0530

    setup home page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,15 @@
+import styles from "../styles/Home.module.css";
+
 export default function Home() {
-  return <div>This is main next JS...</div>;
+  return (
+    <div className={styles.nav}>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Servises</li>
+        <li>Blogs</li>
+      </ul>
+      <h1>Welcome to Hunting coders blogs.</h1>
+    </div>
+  );
 }
--- public/aap.js
+++
@@ -1,1 +0,0 @@
-alert("Enter your age");
