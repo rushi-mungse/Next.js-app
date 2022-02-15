2f6902f2a5c4e12b21adba1239bfc8b7bdacc399
rushi mungse <mungse.rushi@gmail.com>
2022-02-15 23:45:51 +0530

    link component

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,22 @@
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className={styles.nav}>
       <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Servises</li>
-        <li>Blogs</li>
+        <Link href="/">
+          <li>Home</li>
+        </Link>
+        <Link href="/about">
+          <li>About</li>
+        </Link>
+        <Link href="/services">
+          <li>Services</li>
+        </Link>
+        <Link href="/blog">
+          <li>Blogs</li>
+        </Link>
       </ul>
       <h1>Welcome to Hunting coders blogs.</h1>
     </div>
