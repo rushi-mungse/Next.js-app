442a8e1e75cbc888c4c4381ea7cd80bc8e3a650e
rushi mungse <mungse.rushi@gmail.com>
2022-03-05 22:04:49 +0530

    jsx style adding

--- pages/blog.js
+++ pages/blog.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 const blog = () => {
   return <div>blog</div>;
 };
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to Hunting coders blogs.</h1>
-    </div>
+    <>
+      <style jsx>
+        {`
+          .home_page {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+          }
+          h1 {
+            color: red;
+          }
+        `}
+      </style>
+
+      <div className="home_page">
+        <h1>Welcome to Hunting coders blogs.</h1>
+      </div>
+    </>
   );
 }
