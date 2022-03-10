61104752f37983a10c3eab20fe77f52314b91344
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2022-03-10 11:19:42 +0530

    designing blogs page

--- components/ArticleCard.js
+++ components/ArticleCard.js
@@ -2,18 +2,19 @@ import Link from "next/link";
 import styles from "../styles/ArticleCard.module.css";
 const ArticleCard = ({ data }) => {
   return (
-    <div className={styles.articleCard}>
-      <Link href={`/article/${data._id}`}>
+    <Link href={`/article/${data._id}`}>
+      <div className={styles.articleCard}>
         <h1 className={styles.articleTopic}>{data.articleTopic}</h1>
-      </Link>
-      <p className={styles.articleSummary}>
-        {data.articleSummary.substring(0, 150)} ...
-      </p>
-      <div className={styles.articleData}>
-        <span className={styles.articleCreatedAt}>9:56 PM</span>
-        <strong className={styles.articleAuthor}>{data.articleAuthor}</strong>
+
+        <p className={styles.articleSummary}>
+          {data.articleSummary.substring(0, 150)} ...
+        </p>
+        <div className={styles.articleData}>
+          <span className={styles.articleCreatedAt}>9:56 PM</span>
+          <strong className={styles.articleAuthor}>{data.articleAuthor}</strong>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
